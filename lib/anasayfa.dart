f0beb69ed1b8789428e509be64d4f49afa4dae41
Fatih BULUT <bulut.fth.66@outlook.com>
2023-01-17 21:04:15 +0300

    yeni birşeyler işte

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:son_deneme/yanmenu.dart';
 import 'package:english_words/english_words.dart';
+import 'FavoritePage.dart';
 
 class AnaSayfa extends StatefulWidget {
   const AnaSayfa({super.key});
@@ -19,13 +20,60 @@ class _AnaSayfaState extends State<AnaSayfa> {
           style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: Card()
-        ),
-      ),
+      body: const MyStatelessWidget(),
       drawer: const YanMenu(),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const BigCard(),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite),
+                label: const Text('Like'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  //appState.getNext();
+                },
+                child: const Text('Next'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text('baba'),
+      ),
     );
   }
 }
