b37ef369406c3d2d38c35ccb533cf8a94a8fd8fa
Fatih BULUT <bulut.fth.66@outlook.com>
2023-01-31 19:29:33 +0300

    Çöp kutusu aktif oldu

--- lib/page/Garbage_Page.dart
+++ lib/page/Garbage_Page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ilk/provider/Yeni_kelime.dart';
+import 'package:provider/provider.dart';
 
 class GarbagePage extends StatefulWidget {
   const GarbagePage({super.key});
@@ -10,9 +12,35 @@ class GarbagePage extends StatefulWidget {
 class _GarbagePageState extends State<GarbagePage> {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<MyAppState>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Garbage'),
+      ),
+      body: Column(
+        children: [
+          Text('You have '
+              '${provider.garbages.length} Garbages:'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: provider.garbages.length,
+              itemBuilder: (favorite, index) {
+                return ListTile(
+                  title: Text(provider.garbages[index].asPascalCase),
+                  leading: const Icon(Icons.heart_broken),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete_outline),
+                    onPressed: () {
+                      setState(() {
+                        provider.garbages.removeAt(index);
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
